25c56daf354ce1f6f87b1b99fb8884cdb179e444
Kit Pearson <dev_kit@icloud.com>
2013-10-23 14:28:24 -0700

    Added discriptions to businesses.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131004003700) do
+ActiveRecord::Schema.define(version: 20131023212346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20131004003700) do
     t.boolean  "floor"
     t.integer  "user_id"
     t.integer  "building_id"
+    t.string   "discription"
   end
 
   create_table "businesses_categories", id: false, force: true do |t|
